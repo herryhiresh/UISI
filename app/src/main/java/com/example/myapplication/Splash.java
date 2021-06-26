80d4792e9e9704df755f78a11452878b20fb4a62
herryhiresh <herry.hiresh@hotmail.com>
2021-06-26 18:36:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/Splash.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    ProgressBar initLoad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+
+    }
+}
